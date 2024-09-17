8f0b27228f4fe30432f2f92e376c9521998cb19f
Flying_Fish <johann.lacroix@kapsloc.com>
2024-09-17 22:40:17 +0200

    backup 17/09/24

--- toggleLike.js
+++
@@ -1,9 +0,0 @@
-function toggleLike(element) {
-    const whiteHeart = element.querySelector('.white-heart');
-    const purpleHeart = element.querySelector('.purple-heart');
-    
-    // Toggle between showing the white and purple hearts
-    whiteHeart.classList.toggle('hidden');
-    purpleHeart.classList.toggle('hidden');
-  }
-  
